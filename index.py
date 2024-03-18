a8525ea9119e5e088e517bf68161b18c59449f46
quanxily <yangquanxi80@gmail.com>
2024-03-18 21:02:16 +0800

    make it better

---
+++ index.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template,request
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    x ="我和翔順寶寶的紀念網站<br>"
+    x +="<a href=/love>一周年紀念日</a><br>"
+    x +="<a href=/our>彼此的基本資料</a><br>"
+    x +="<a href=/story>相識到相戀故事</a><br>"
+    x +="<a href=/photo>照片牆</a><br>"
+    x +="<a href=/play>澎湖一日遊</a><br>"
+    return x
+
+@app.route("/love")
+def love():
+    return render_template("love.html")
+
+@app.route("/our")
+def our():
+    return render_template("our.html")
+
+@app.route("/story")
+def story():
+    return render_template("story.html")
+
+@app.route("/photo")
+def photo():
+    return render_template("photo.html")
+
+@app.route("/play")
+def play():
+    return render_template("play.html")
+
+
+if __name__ == "__main__":
+    app.run()
